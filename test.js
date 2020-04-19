2c5da8171edc87bd0409d6a65ddc7bc013882c68
Narendra Malviya <narendramalviya5@gmail.com>
2020-04-19 23:23:03 +0530

    test commit

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 const array1 = [5, 12, 8, 130, 44];
 
-const found = array1.find(element => element > 10);
-console.log('this is excu');
-console.log(found);
+const fun = () =>{
+     console.log("Hello wolrd");
+     
+}
 // expected output: 12
